3df4c29a36a79db0c41b9a5d025ef94367a3c310
ed-dole <mintana2568@gmail.com>
2026-09-11 10:50:16 +0700

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+
+  window.APP_CONFIG = Object.freeze({
+    API_URL: 'https://script.google.com/macros/s/AKfycbwoLRdbDSbuIF_Ra7ZCcGMq49I_hMC-DIWC78o85V_Av1UM8YdybyNtw4jlrUSTuhqf/exec'
+  });
+})();
