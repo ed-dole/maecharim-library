f738800808995de1774d053c6184066d2eb4130f
ed-dole <mintana2568@gmail.com>
2026-09-11 11:20:27 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbwoLRdbDSbuIF_Ra7ZCcGMq49I_hMC-DIWC78o85V_Av1UM8YdybyNtw4jlrUSTuhqf/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbypfiiaelw-xUpzZfqIFbKBjKzzJ7gn3uwqsqLTxTa6zVlRhG7fcEkF5BSpknKTglIeMg/exec'
   });
 })();
